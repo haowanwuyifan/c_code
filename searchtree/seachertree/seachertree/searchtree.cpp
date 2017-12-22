d4a4a95cda8fee51e2c4531ab7aaca3f570c57b1
一凡 吴 <823348523@qq.com>
2017-12-22 09:17:12 +0800

    实验八搜索树

---
+++ searchtree/seachertree/seachertree/searchtree.cpp
@@ -0,0 +1,178 @@
+#include<iostream>
+#include<stdlib.h>
+
+using namespace std;
+
+struct node
+{
+	pair<int, char>  element;
+	node* leftchild;
+	node* rightchild;
+	int index;
+};
+
+class searchtree
+{
+public:
+	searchtree(int size);
+	void buildtree(int* a);
+	void insert(int);
+	void erase(int);
+	node* getfirst();
+	node* getsecond();
+	node* getindex();
+	void middleorder(node* t);
+	node* getroot();
+protected:
+	node * root;
+	int height;
+	int listsize;
+	int tsize;
+
+};
+
+searchtree::searchtree(int size)
+{
+	root = NULL;
+	height = 0;
+	listsize = 0;
+	tsize = size;
+}
+
+void searchtree::buildtree(int* a)
+{
+	root = new node;
+	root->element.first = a[0];
+	root->element.second = (char)(a[0] + 96);
+	root->leftchild = root->rightchild = NULL;
+	for (int i = 1; i < tsize; i++)
+	{
+		node* current = root;
+		while (1)
+		{
+			if (a[i] < current->element.first&&current->leftchild != NULL)
+			{
+				current = current->leftchild;
+			}
+			if (a[i] > current->element.first&&current->rightchild != NULL)
+			{
+				current = current->rightchild;
+			}
+			if (a[i] < current->element.first&&current->leftchild == NULL)
+			{
+				current->leftchild = new node;
+				current->leftchild->element.first = a[i];
+				current->leftchild->element.second = (char)(a[i] + 96);
+				current->leftchild->leftchild = current->leftchild->rightchild = NULL;
+				//i++;
+				break;
+			}
+			if (a[i] > current->element.first&& current->rightchild == NULL)
+			{
+				current->rightchild = new node;
+				current->rightchild->element.first = a[i];
+				current->rightchild->element.second = (char)(a[i] + 96);
+				current->rightchild->leftchild = current->rightchild->rightchild = NULL;
+				//i++;
+				break;
+			}
+		}
+
+
+	}
+}
+
+void searchtree::insert(int key)
+{
+	node* store = new node;
+	store->element.first = key;
+	store->element.second = (char)(key + 96);
+	store->leftchild = store->rightchild = NULL;
+	node* current = root;
+	//current->element.first = key;
+	while (1)
+	{
+		if (current->element.first == key)
+		{
+			current->element.second = (char)(key + 96);
+			break;
+		}
+		if (current->element.first > key&&current->leftchild != NULL)
+		{
+			current = current->leftchild;
+			//break;
+		}
+		if (current->element.first < key&&current->rightchild != NULL)
+		{
+			current = current->rightchild;
+			//break;
+		}
+		if (key < current->element.first&&current->leftchild == NULL)
+		{
+			current->leftchild = store;
+			break;
+		}
+		if (key > current->element.first&&current->rightchild == NULL)
+		{
+			current->rightchild = store;
+			break;
+		}
+	}
+	tsize++;
+}
+
+//void searchtree::erase(int key)
+//{
+//	node* current = root;
+//	node* back = current;
+//	while (1)
+//	{
+//		if (current->element.first == key)
+//		{
+//			if(current.)
+//		}
+//	}
+//}
+
+
+
+
+
+void searchtree::middleorder(node* t)
+{
+	if (t)
+	{
+		cout << t->element.second << ' ';
+		middleorder(t->leftchild);
+		middleorder(t->rightchild);
+	}
+}
+
+node* searchtree::getroot()
+{
+	return root;
+}
+
+int main()
+{
+	int n;
+	cout << "input the number of size:";
+	cin >> n;
+	int* a = new int[n];
+	cout << "input the keys: ";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a[i];
+	}
+	searchtree st(n);
+	st.buildtree(a);
+	st.middleorder(st.getroot());
+	int ins;
+	cout << "if insert " ;
+	cin >> ins;
+	cout << "then ";
+	st.insert(ins);
+	st.middleorder(st.getroot());
+	system("pause");
+	return 0;
+}
